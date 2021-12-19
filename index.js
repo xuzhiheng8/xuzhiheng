3096c339ec24863dd9c39fa992b1e8f906c8f0bd
xuzhiheng8 <89857542+xuzhiheng8@users.noreply.github.com>
2021-12-19 12:06:56 +0800

    Add files via upload
    第二次作业

--- index.js
+++ index.js
@@ -1,39 +1,123 @@
-function Mine(tr,td,mineNum){
-    this.tr=tr;
-    this.td=td;
-    this.mineNum=mineNum;
+function Mine(tr, td, mineNum) {
+    this.tr = tr; 
+    this.td = td; 
+    this.mineNum = mineNum; 
 
-    this.squares=[];
-    this.tds=[];
-    this.surplusMine=mineNum;
-    this.allRight=false;
+    this.squares = [];
+    this.tds = []; 
+    this.surplusMine = mineNum; 
+    this.allRight = false; 
 
-    this.parent=document.querySelector('.gameBox');
+    this.parent = document.querySelector('.gameBox');
+
+
 }
 
 
-Mine.prototype.createDom=function(){
-    let table=document.createElement('table');
-    for(let i=0;i<this.tr;i++){
-        let domTr=document.createElement('Tr');
-        this.tds[i]=[];
+Mine.prototype.randomNum = function() {
+    let square = new Array(this.tr * this.td), 
+        len = square.length;
+    for (let i = 0; i < square.length; i++) {
+        square[i] = i;
+    }
+    square.sort(function() {
+        return 0.5 - Math.random()
+    });
+    return square.slice(0, this.mineNum);
+}
+Mine.prototype.init = function() {
+    let rn = this.randomNum(); 
+    let n = 0; 
+    for (let i = 0; i < this.tr; i++) {
+        this.squares[i] = [];
+        for (let j = 0; j < this.td; j++) {
+        
 
-        for(let j=0;j<this.td;j++){
-            let domTd=document.createElement('td');
+            if (rn.indexOf(++n) != -1) {
+              
+                this.squares[i][j] = {
+                    type: 'mine',
+                    x: j,
+                    y: i
+                };
+            } else {
+                this.squares[i][j] = {
+                    type: 'number',
+                    x: j,
+                    y: i,
+                    value: 0
+                };
+            }
+        }
+    }
 
-            this.tds[i][j]=domTd;
+
+    this.updateNum();
+    this.createDom();
+
+    this.parent.oncontextmenu = function() {
+        return false;
+    }
+
+
+    this.mineNumDom = document.querySelector('.mineNum');
+    this.mineNumDom.innerHTML = this.surplusMine;
+
+};
+
+
+Mine.prototype.createDom = function() {
+    let self = this;
+    let table = document.createElement('table');
+
+    for (let i = 0; i < this.tr; i++) { 
+        let domTr = document.createElement('tr');
+        this.tds[i] = [];
+
+        for (let j = 0; j < this.td; j++) {
+            let domTd = document.createElement('td');
+
+            domTd.pos = [i, j]; 
+            domTd.onmousedown = function(event) {
+                self.play(event, this); 
+            };
+
+            this.tds[i][j] = domTd; 
+
+         
+
 
             domTr.appendChild(domTd);
         }
         table.appendChild(domTr);
     }
-    this .parent.appendChild(table);
+    this.parent.innerHTML = '';
+    this.parent.appendChild(table);
+};
+
+
+Mine.prototype.getAround = function(square) {
+    let x = square.x;
+    let y = square.y;
+    let result = []; 
 
 
 
+    for (let i = x - 1; i <= x + 1; i++) {
+        for (let j = y - 1; j <= y + 1; j++) {
+            if (
+                i < 0 || 
+                j < 0 || 
+                i > this.td - 1 || 
+                j > this.tr - 1 || 
+                (i == x && j == y) || 
+                this.squares[j][i].type == 'mine' 
+            ) {
+                continue;
+            }
 
-
-
-
-let mine=new Mine(28,28,99);
-mine.createDom();
+            result.push([j, i]); 
+        }
+    }
+    return result;
+};
